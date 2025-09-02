e5872f862ee47099337498e20f3b4835f1460138
Nydaxe <james.salehpour@gmail.com>
2025-09-01 21:01:01 -0400

    MADE TUBES PLACE
    tubes place slightly correctly

--- AutomationGame/Assets/Scripts/TubePlacing.cs
+++ AutomationGame/Assets/Scripts/TubePlacing.cs
@@ -1,49 +1,92 @@
+using System;
 using System.Collections;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class TubePlacing : MonoBehaviour
 {
+    public LineRenderer previewTubeLine;
+    public LineRenderer tubeLine;
+
     public bool placingtube = false;
-    int tubePlacements;
 
     public IEnumerator EnableTubePlacing()
     {        
         placingtube = true;
 
-        GameObject tube = TubeManager.Instance.GetComponent<ObjectPool>().GetObject();
-        LineRenderer tubeLine = tube.GetComponent<LineRenderer>();
-
         tubeLine.positionCount = 1;
-
-        tubePlacements = 0;
 
         bool initialPlacement = false;
 
+        Vector2 lastPipePosition = Vector2.zero;
+        
+        Vector2 corner = Vector2.zero;
+
+        bool prioritizeX = true;
 
         while (placingtube)
         {
-            //Gets the mouse position in world space
-            Vector3 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-            mouseScreenPosition.z = 0f;
+            yield return null;
+
+            Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
 
             if (Mouse.current.leftButton.wasPressedThisFrame && !initialPlacement)
             {
                 tubeLine.SetPosition(0, mouseScreenPosition);
+                lastPipePosition = mouseScreenPosition;
+
+                previewTubeLine.positionCount = 3;
+                previewTubeLine.SetPosition(0, mouseScreenPosition);
+                previewTubeLine.SetPosition(1, mouseScreenPosition);
+                previewTubeLine.SetPosition(2, mouseScreenPosition);
+
                 initialPlacement = true;
                 Debug.Log("Initial Placement");
                 continue;
             }
 
             if (!initialPlacement)
-            {
                 continue;
+
+            // Use the last placed point
+            lastPipePosition = tubeLine.GetPosition(tubeLine.positionCount - 1);
+
+            float xDistance = Mathf.Abs(mouseScreenPosition.x - lastPipePosition.x);
+            float yDistance = Mathf.Abs(mouseScreenPosition.y - lastPipePosition.y);
+
+            prioritizeX = xDistance >= yDistance;
+
+            if (tubeLine.positionCount > 1)
+            {
+                // Change direction if overlapping in the same axis
+                int lastPipeDirectionX = MathF.Sign(lastPipePosition.x - tubeLine.GetPosition(tubeLine.positionCount - 2).x);
+                int lastPipeDirectionY = MathF.Sign(lastPipePosition.y - tubeLine.GetPosition(tubeLine.positionCount - 2).y);
+
+                if (lastPipeDirectionX != 0)
+                    prioritizeX = lastPipeDirectionX == MathF.Sign(mouseScreenPosition.x - lastPipePosition.x);
+                if (lastPipeDirectionY != 0)
+                    prioritizeX = !(lastPipeDirectionY == MathF.Sign(mouseScreenPosition.y - lastPipePosition.y));
             }
 
-            Debug.Log("yo");
+            // Calculate corner position
+                corner = prioritizeX
+                ? new Vector2(mouseScreenPosition.x, lastPipePosition.y)
+                : new Vector2(lastPipePosition.x, mouseScreenPosition.y);
 
-            yield return null;
+            // For preview, always use the last placed point
+            previewTubeLine.SetPosition(0, lastPipePosition);
+            previewTubeLine.SetPosition(1, corner);
+            previewTubeLine.SetPosition(2, mouseScreenPosition);
+
+            // On click, add the corner and mouse as new points
+            if (Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                tubeLine.positionCount += 2;
+                tubeLine.SetPosition(tubeLine.positionCount - 2, corner);
+                tubeLine.SetPosition(tubeLine.positionCount - 1, mouseScreenPosition);
+            }
         }
     }
 }
