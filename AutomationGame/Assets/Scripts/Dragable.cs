baa7aff0312d3e908bc70f0373a46815e9c2372c
Nydaxe <james.salehpour@gmail.com>
2025-09-05 19:22:23 -0400

    Dragging not done yet
    just FYI

--- AutomationGame/Assets/Scripts/Dragable.cs
+++ AutomationGame/Assets/Scripts/Dragable.cs
@@ -1,6 +1,19 @@
 using UnityEngine;
+using System.Collections;
 
-public class Dragable
+public class Dragable : MonoBehaviour
 {
-    
+    void OnMouseDrag()
+    {
+        Vector2 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        if (StateManager.state != StateManager.State.Idle)
+        {
+            if ((Vector2)transform.position == mouseWorldPosition)
+                return;
+
+            transform.position = Vector2.Lerp(transform.position, mouseWorldPosition, 0.2f);
+        }
+    }
+
 }
